ed6ee04955513ca8cad92fae67e1d136304509a3
backtobackend226 <backtobackend@proton.me>
2026-02-03 13:56:41 -0800

    jest config

---
+++ apps/gate/jest.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/../$1',
+    '^@lib/(.*)$': '<rootDir>/../../libs/$1',
+  },
+};
