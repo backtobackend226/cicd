9984935772e5b6c1869eb32c9eae1b7c5eadc907
backtobackend226 <backtobackend@proton.me>
2026-02-10 14:50:19 +0200

    sdfsdfsdf

--- apps/gate/src/users/api/users.controller.ts
+++ apps/gate/src/users/api/users.controller.ts
@@ -31,8 +31,8 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string) {
+    return this.usersService.update(+id);
   }
 
   @Delete(':id')
